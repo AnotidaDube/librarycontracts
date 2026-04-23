a3a3766aecc2dc5d264d09da7b6e783cab1da1bd
Anotida Dube <dubeanotida5@gmail.com>
2026-04-23 14:08:43 +0200

    Switch to PostgreSQL

--- library_contract/settings.py
+++ library_contract/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'library_contract.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'library_contracts_db'),
+        'USER': os.environ.get('DB_USER', 'contracts_admin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'Contracts@library2026'), 
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
