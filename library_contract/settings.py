dd0525725772b6e212cc14ecf8b45818183b8f92
Anotida Dube <dubeanotida5@gmail.com>
2026-04-23 12:09:18 +0200

    Fix static files and trigger deployment

--- library_contract/settings.py
+++ library_contract/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files (Uploaded Contracts)
